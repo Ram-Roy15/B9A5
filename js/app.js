d1e14271208c7d46cb200152e45963e7457625b4
Ram-Roy15 <ramroycombd.com>
2024-02-19 01:06:25 +0600

    resolved3

--- js/app.js
+++ js/app.js
@@ -6,20 +6,37 @@ const allSeats = document.getElementsByClassName("seat");
 let bookedSeats = 0;
 let availableSeats = 40;
 let totalAmount = 0;
+let grandTotal = 0;
 for (const seat of allSeats) {
   seat.addEventListener("click", function (e) {
     e.target.style.backgroundColor = "#1cd100ff";
-
+    const tikitNumber = e.target.innerText;
+    console.log(tikitNumber);
     totalAmount = totalAmount + 550;
     if (bookedSeats < 40) {
       bookedSeats = bookedSeats + 1;
+      grandTotal = grandTotal + 550;
       setInnerText("booked-seat", bookedSeats);
+      setInnerText("grand-total", grandTotal);
     } else {
       alert("No seat available");
     }
     availableSeats = 40 - bookedSeats;
     setInnerText("availavble-seat", availableSeats);
     setInnerText("total-price", totalAmount);
+
+    const tikitContainer = document.getElementById("tikit-container");
+    const li = document.createElement("li");
+    const p1 = document.createElement("p");
+    p1.innerText = tikitNumber;
+    li.appendChild(p1);
+    const p2 = document.createElement("p");
+    p2.innerText = "Economy";
+    li.appendChild(p2);
+    const p3 = document.createElement("p");
+    p3.innerText = "550";
+    li.appendChild(p3);
+    tikitContainer.appendChild(li);
   });
 }
 
